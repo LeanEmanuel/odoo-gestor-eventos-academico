3a6620dcbe28fff12cfbd0d69de0fe8dda1deefc
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2025-03-26 00:01:05 +0100

    feat(tag): add views and menu entry for event tag model

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        'views/tag_views.xml',
         'views/category_views.xml',
         'views/assistant_views.xml',
         'views/event_views.xml',
